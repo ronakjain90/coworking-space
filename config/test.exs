277532a86f45fb62fe0c9ec6b0413c1834c2f5b9
Ronak <ronakjain90@gmail.com>
2016-08-12 15:11:45 +0530

    Fixed Error Tests

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :coworking_space, CoworkingSpace.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "ronak",
+  password: "",
   database: "coworking_space_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
