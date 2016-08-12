6aa0f01ff17e195948487079e37399c0bce8c9e5
Ronak <ronakjain90@gmail.com>
2016-08-12 12:38:23 +0530

    Changes

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -4,6 +4,6 @@ defmodule CoworkingSpace.SessionController do
   def index(conn, _params) do
     # Return some static JSON for now
     conn
-    |> json(%{status: "Ok"})
+    |> json(%{status: "Ok ok"})
   end
 end
